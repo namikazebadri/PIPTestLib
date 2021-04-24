d2b16da30f17fc60b6dbe32e83962b823bda129a
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-24 11:33:11 +0700

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pylib_namikaze",
-    version="1.0.0",
+    version="1.0.1",
     author="Unis Badri",
     author_email="unis.badri@elementcreativestudio.com",
     description="Test library example for pypi.",
