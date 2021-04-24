30303b0fa253543bd5ab5c2b650fc6366d532fa9
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-24 14:06:56 +0700

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pylib_namikaze",
+    name="py-mannerism",
     version="1.0.2",
     author="Unis Badri",
     author_email="unis.badri@elementcreativestudio.com",
