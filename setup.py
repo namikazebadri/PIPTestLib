36a3184c0360f10047407945d88b4141134bcd0f
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-24 11:29:57 +0700

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="namikaze_pylib",
+    name="pylib_namikaze",
     version="1.0.0",
     author="Unis Badri",
     author_email="unis.badri@elementcreativestudio.com",
