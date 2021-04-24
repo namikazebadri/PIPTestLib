6ad010e88ed3faa232adc144b7e1089d49935224
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-24 11:12:13 +0700

    change package name.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pylib",
+    name="Pylib",
     version="1.0.0",
     author="Unis Badri",
     author_email="unis.badri@elementcreativestudio.com",
