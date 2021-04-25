07fd6d22b017706fa673150353862b7bfc933520
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-25 19:26:37 +0700

    increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-mannerism",
-    version="1.0.2",
+    version="1.0.3",
     author="Unis Badri",
     author_email="unis.badri@elementcreativestudio.com",
     description="Test library example for pypi.",
